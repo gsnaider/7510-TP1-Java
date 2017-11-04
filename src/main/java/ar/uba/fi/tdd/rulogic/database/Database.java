e9fe3306499b59616c28564aff40f7c63c230803
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-04 18:50:35 -0300

    Add database constructor

--- src/main/java/ar/uba/fi/tdd/rulogic/database/Database.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/database/Database.java
@@ -1,8 +1,15 @@
 package ar.uba.fi.tdd.rulogic.database;
 
+import org.junit.Rule;
+import com.google.common.collect.ImmutableSet;
+import ar.uba.fi.tdd.rulogic.model.Fact;
 import ar.uba.fi.tdd.rulogic.model.Statement;
 
 public class Database {
+
+  public Database(ImmutableSet<Fact> facts, ImmutableSet<Rule> rules) {
+    // TODO
+  }
 
   public boolean contains(Statement statement) {
     // TODO
