c748ca961d434b0471dbdaa7501686121c53def7
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-04 01:45:41 -0300

    Rename KnowledgeBaseTest to ParentDatabaseTest

--- src/test/java/ar/uba/fi/tdd/rulogic/model/ParentDatabaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/ParentDatabaseTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 
-public class KnowledgeBaseTest {
+public class ParentDatabaseTest {
 
   private KnowledgeBase knowledgeBase;
 
