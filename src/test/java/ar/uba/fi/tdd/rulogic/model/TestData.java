71f6b337fe5bbebb318a1f7cddccd223d636357a
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-04 19:43:55 -0300

    Refactor database creation

--- src/test/java/ar/uba/fi/tdd/rulogic/model/TestData.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/TestData.java
@@ -10,15 +10,22 @@ public final class TestData {
 
   public static final Database EMPTY_DATABASE = new Database(ImmutableSet.of(), ImmutableSet.of());
 
-  public static final Database SMALL_DATABASE =
-      new Database(
-          ImmutableSet.of(
-            new Fact("varon", ImmutableList.of("juan")),
-            new Fact("padre", ImmutableList.of("juan", "pepe"))),
-          ImmutableSet.of(
-              new Rule(
-                "hijo", ImmutableList.of("X", "Y"), ImmutableSet.of(
-                new Fact("varon", ImmutableList.of("X")),
-                new Fact("padre", ImmutableList.of("Y", "X"))))));
+  public static final Database SMALL_DATABASE = smallDatabase();
+
+  private static Database smallDatabase() {
+    ImmutableSet<Fact> facts = ImmutableSet.of(
+        new Fact("varon", ImmutableList.of("juan")),
+        new Fact("padre", ImmutableList.of("juan", "pepe")));
+
+    String ruleName = "hijo";
+    ImmutableList<String> ruleParams = ImmutableList.of("X", "Y");
+    ImmutableSet<Statement> ruleStatements = ImmutableSet.of(
+        new Fact("varon", ImmutableList.of("X")),
+        new Fact("padre", ImmutableList.of("Y", "X")));
+    ImmutableSet<Rule> rules = ImmutableSet.of(new Rule(ruleName, ruleParams, ruleStatements));
+
+    return new Database(facts, rules);
+  }
+
 
 }
