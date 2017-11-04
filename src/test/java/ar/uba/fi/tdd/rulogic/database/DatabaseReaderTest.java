20d41a6581d934a64d023efe95581964809b41d8
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-04 19:37:07 -0300

    Add DatabaseReader tests

--- src/test/java/ar/uba/fi/tdd/rulogic/database/DatabaseReaderTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/database/DatabaseReaderTest.java
@@ -1,17 +1,39 @@
 package ar.uba.fi.tdd.rulogic.database;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import org.junit.Before;
 import org.junit.Test;
-import ar.uba.fi.tdd.rulogic.database.DatabaseReader;
-import ar.uba.fi.tdd.rulogic.database.IllegalDatabaseFormatException;
+import ar.uba.fi.tdd.rulogic.model.TestData;
 
-public class InvalidDatabaseTest {
+public class DatabaseReaderTest {
 
+  private static final String EMPTY_DATABASE_PATH = "empty.db";
+  private static final String SMALL_DATABASE_PATH = "small.db";
   private static final String INVALID_RULE_DATABASE_PATH = "invalid-rule.db";
   private static final String INVALID_STATEMENT_DATABASE_PATH = "invalid-statement.db";
 
+  private DatabaseReader databaseReader;
+
+  @Before
+  public void setUp() {
+    databaseReader = new DatabaseReader();
+  }
+
   @Test
-  public void testNewKnowledgeBase_DatabaseWithInvalidRule_ThrowsIllegalDatabaseFormatException() {
+  public void testReadDatabase_EmptyDatabase_ReturnsEmptyDatabase() {
+    Database emptyDatabase = databaseReader.readDatabase(EMPTY_DATABASE_PATH);
+    assertThat(emptyDatabase).isEqualTo(TestData.EMPTY_DATABASE);
+  }
+
+  @Test
+  public void testReadDatabase_ValidDatabase_ReturnsParsedDatabase() {
+    Database database = databaseReader.readDatabase(SMALL_DATABASE_PATH);
+    assertThat(database).isEqualTo(TestData.SMALL_DATABASE);
+  }
+
+  @Test
+  public void testReadDatabase_DatabaseWithInvalidRule_ThrowsIllegalDatabaseFormatException() {
     assertThatExceptionOfType(IllegalDatabaseFormatException.class).isThrownBy(() -> {
       DatabaseReader databaseReader = new DatabaseReader();
       databaseReader.readDatabase(INVALID_RULE_DATABASE_PATH);
@@ -20,7 +42,7 @@ public class InvalidDatabaseTest {
   }
 
   @Test
-  public void testNewKnowledgeBase_DatabaseWithInvalidStatement_ThrowsIllegalDatabaseFormatException() {
+  public void testReadDatabase_DatabaseWithInvalidStatement_ThrowsIllegalDatabaseFormatException() {
     assertThatExceptionOfType(IllegalDatabaseFormatException.class).isThrownBy(() -> {
       DatabaseReader databaseReader = new DatabaseReader();
       databaseReader.readDatabase(INVALID_STATEMENT_DATABASE_PATH);
