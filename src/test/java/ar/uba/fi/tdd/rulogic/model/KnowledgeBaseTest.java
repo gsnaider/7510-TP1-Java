3a9ccd290d9b600df2d08ce40e41ba97abfeaeb1
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-04 01:14:21 -0300

    Add integration tests for KnowledgeBase

--- src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
@@ -1,26 +1,49 @@
 package ar.uba.fi.tdd.rulogic.model;
 
-import static org.mockito.MockitoAnnotations.initMocks;
-import org.junit.Assert;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 
 public class KnowledgeBaseTest {
 
-  @InjectMocks
   private KnowledgeBase knowledgeBase;
 
   @Before
   public void setUp() throws Exception {
-    initMocks(this);
+    knowledgeBase = new KnowledgeBase();
   }
 
   @Test
-  public void test() {
+  public void testAnswer_PresentFact_ReturnsTrue() {
+    assertThat(knowledgeBase.answer("varon(juan)")).isTrue();
+    assertThat(knowledgeBase.answer("mujer(maria)")).isTrue();
+    assertThat(knowledgeBase.answer("padre(juan, pepe)")).isTrue();
+    assertThat(knowledgeBase.answer("padre(roberto, cecilia)")).isTrue();
+    assertThat(knowledgeBase.answer("hermano(nicolas, roberto)")).isTrue();
+  }
 
-    Assert.assertTrue(this.knowledgeBase.answer("varon (javier)."));
+  @Test
+  public void testAnswer_NonPresentFact_ReturnsFalse() {
+    assertThat(knowledgeBase.answer("mujer(juan)")).isFalse();
+    assertThat(knowledgeBase.answer("varon(maria)")).isFalse();
+    assertThat(knowledgeBase.answer("padre(pepe, juan)")).isFalse();
+    assertThat(knowledgeBase.answer("padre(hector, cecilia)")).isFalse();
+    assertThat(knowledgeBase.answer("hermano(pepe, juan)")).isFalse();
+    assertThat(knowledgeBase.answer("hermana(maria, juan)")).isFalse();
+  }
 
+  @Test
+  public void testAnswer_PresentRule_ReturnsTrue() {
+    assertThat(knowledgeBase.answer("hijo(pepe, juan)")).isTrue();
+    assertThat(knowledgeBase.answer("hija(maria, hector)")).isTrue();
+    assertThat(knowledgeBase.answer("tio(nicolas, alejandro, roberto)")).isTrue();
+  }
+
+  @Test
+  public void testAnswer_NonPresentRule_ReturnsFalse() {
+    assertThat(knowledgeBase.answer("hijo(pepe, hector)")).isFalse();
+    assertThat(knowledgeBase.answer("hija(maria, roberto)")).isFalse();
+    assertThat(knowledgeBase.answer("tia(julia, alejandro, roberto)")).isFalse();
   }
 
 }
