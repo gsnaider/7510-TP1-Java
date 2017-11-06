0fa69394d3e0933d12be9b55e3e3483757777243
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-06 05:05:41 -0300

    Complete query validator logic

--- src/main/java/ar/uba/fi/tdd/rulogic/validator/QueryValidator.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/validator/QueryValidator.java
@@ -1,13 +1,16 @@
 package ar.uba.fi.tdd.rulogic.validator;
 
+import java.util.regex.Pattern;
 import ar.uba.fi.tdd.rulogic.parser.Validator;
 
 public class QueryValidator implements Validator<String> {
 
+  private static final String QUERY_REGEX = "^\\w+\\((\\w+)(,\\w+)*\\)$";
+
   @Override
-  public boolean isValid(String t) {
-    // TODO Auto-generated method stub
-    return false;
+  public boolean isValid(String query) {
+    String queryWithoutWhitespace = query.replaceAll("\\s", "");
+    return Pattern.matches(QUERY_REGEX, queryWithoutWhitespace);
   }
 
 }
