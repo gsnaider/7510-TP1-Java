85db7d05a4d8a0a7b8893039334347a08fd81dbb
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-06 05:24:50 -0300

    Add database tests

--- src/test/java/ar/uba/fi/tdd/rulogic/model/QueryTestData.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/QueryTestData.java
@@ -14,6 +14,17 @@ public final class QueryTestData {
   public static final Query QUERY_2 =
       Query.builder().name("padre").parameters(ImmutableList.of("juan", "pepe")).build();
 
+  public static final String QUERY_3_STRING = "hijo(pepe, juan)";
+  public static final Query QUERY_3 =
+      Query.builder().name("hijo").parameters(ImmutableList.of("pepe", "juan")).build();
+
+  public static final Query NON_PRESENT_QUERY_1 =
+      Query.builder().name("varon").parameters(ImmutableList.of("maria")).build();
+  public static final Query NON_PRESENT_QUERY_2 =
+      Query.builder().name("hijo").parameters(ImmutableList.of("roberto", "mario")).build();
+  public static final Query NON_PRESENT_QUERY_3 =
+      Query.builder().name("hija").parameters(ImmutableList.of("maria", "pepe")).build();
+
   public static final String INVALID_QUERY_STRING = "varon";
 
 }
