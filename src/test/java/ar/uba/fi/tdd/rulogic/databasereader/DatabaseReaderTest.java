50b01262209d69711d7cec5dff838d4fa6901091
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-05 21:22:32 -0300

    Rename database reader test package

--- src/test/java/ar/uba/fi/tdd/rulogic/databasereader/DatabaseReaderTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/databasereader/DatabaseReaderTest.java
@@ -1,4 +1,4 @@
-package ar.uba.fi.tdd.rulogic.database;
+package ar.uba.fi.tdd.rulogic.databasereader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
